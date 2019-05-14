0a140a9001c072be5583df026a889422a5d6c885
kevin lang <langkevina@gmail.com>
2019-05-14 23:37:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ end
 
 def words_five_letters_long(text)
   # return text.scan(/^\w{5}\s/)
-  return text.scan(/\w{1,10}\b/)
+  return text.scan(/\w{1,5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
