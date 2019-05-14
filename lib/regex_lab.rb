b92e689ac67916707ca354b0fd65415708bbd0f2
kevin lang <langkevina@gmail.com>
2019-05-14 23:41:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ end
 
   def words_five_letters_long(text)
   # return text.scan(/^\w{5}\s/)
-  return text.scan(/\b\w{1,5}\b/)
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
