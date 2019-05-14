5b3ecf27a76fdcdebfa817590c4fe4080d3bf316
kevin lang <langkevina@gmail.com>
2019-05-14 22:15:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  s = word.delete(' ')
+  if s.match(/\A[aeiuo]/) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text =~ /\A[aeiuo]/
 end
 
 def words_five_letters_long(text)
